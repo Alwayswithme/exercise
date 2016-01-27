7b5358b17f30222fd998fbf2c46b63759d8b6abc
Alwayswithme <phx13ye@gmail.com>
2016-01-27 17:58:49 +0800

    抓取ishadowsocks密码的脚本

---
+++ ishadow.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+import re
+import json
+import urllib.request
+
+file = "/home/ye/Desktop/ss.json"
+url = "http://www.ishadowsocks.com/"
+pIdx = "C"
+
+obj = urllib.request.urlopen(url)
+content = obj.read().decode("utf8")
+
+matcher = re.compile(r"([ABC])密码:(.*?)<")
+
+passwd = {m.group(1): m.group(2) for m in matcher.finditer(content)}
+print(passwd)
+
+try:
+    with open(file, "r") as f:
+        data = json.load(f)
+        data['password'] = passwd[pIdx]
+
+    with open(file, "w") as f:
+        json.dump(data, f)
+except IOError:
+    pass
